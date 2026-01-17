353627bd47b3183e01b3b7bb9b40bae42a7b00a9
println <1585951+println@users.noreply.github.com>
2026-01-17 20:22:58 -0300

    feat: Add education offer component, routing, and segmentation HTML.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { CompaniesComponent } from './components/offers/companies/companies.component';
 import { EducationComponent } from './components/offers/education/education.component';
-import { GovernmentComponent } from './components/offers/government/government.component';
 
 export const routes: Routes = [
     {
@@ -11,7 +10,6 @@ export const routes: Routes = [
         children: [
             { path: 'empresa', component: CompaniesComponent },
             { path: 'escola', component: EducationComponent },
-            { path: 'governo', component: GovernmentComponent },
             { path: '', redirectTo: 'empresa', pathMatch: 'full' }
         ]
     }
