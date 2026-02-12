40ca150bbaa3cd594ca43b038dd234132bbb17b7
println <ID+println@users.noreply.github.com>
2026-02-12 19:30:42 -0300

    feat: add initial services page HTML structure with hero, service details, and CTA sections.

--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ROUTES } from '../../config/routes.config';
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-services',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './services.html',
     styleUrl: './services.scss',
 })
