a6c7a7a68b2652a51f7508be182d627e45a6a08a
println <ID+println@users.noreply.github.com>
2026-02-18 20:02:24 -0300

    feat: Add new About and Services pages with their respective components and templates.

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Cta } from '../../components/cta/cta';
 import { Authority } from '../../components/proofs/authority/authority';
+import { UiSection, UiSectionBackgroundDirective, UiSectionHeaderDirective } from '../../components/shared/ui-section/ui-section';
 
 @Component({
     selector: 'app-about',
     standalone: true,
-    imports: [Cta, Authority],
+    imports: [Cta, Authority, UiSection, UiSectionHeaderDirective, UiSectionBackgroundDirective],
     templateUrl: './about.html',
     styleUrl: './about.scss',
 })
--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ROUTES } from '../../config/routes.config';
 import { RouterLink } from "@angular/router";
+import { UiSection, UiSectionBackgroundDirective, UiSectionHeaderDirective } from '../../components/shared/ui-section/ui-section';
 
 @Component({
     selector: 'app-services',
     standalone: true,
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, UiSection, UiSectionHeaderDirective, UiSectionBackgroundDirective],
     templateUrl: './services.html',
     styleUrl: './services.scss',
 })
